089853c3f34b59b42fd53c15fd92d28cc8529441
Jonny Walter <jonny.walter@ownit.nu>
2022-12-27 11:04:51 +0100

    Added Vehicle class

---
+++ Models/Entities/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿namespace GarageMVC.Models.Entities
+{
+    public class Vehicle
+    {
+        public int Id
+        {
+            get; set;
+        }
+        public string? VehicleType
+        {
+            get; set;
+        }
+
+        public string? RegNo
+        {
+            get; set;
+        }
+        public string? Color
+        {
+            get; set;
+        }
+
+        public string? Brand 
+        {
+            get; set;
+        }
+        public string? Model
+        {
+            get; set;
+        }
+        public int NoOfWheels
+        {
+            get; set;
+        }
+        public DateTime TimeOfArrival
+        {
+            get; set;
+        }
+    }
+}
